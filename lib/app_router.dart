16ec2f5938e60e16b699e90dabf185af02a35fb6
Nagi Mohamed <117310405+nagiElshershaby@users.noreply.github.com>
2023-05-20 09:18:25 +0300

    Added Block provider

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,21 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'constants/strings.dart';
+
+import 'package:cats_app/data/repositories/cats_repository.dart';
+import 'package:cats_app/data/web_services/cats_web_services.dart';
+
+import 'package:cats_app/business_logic/cubit/cats_cubit.dart';
 
 import 'presentation/screens/cats_details_screen.dart';
 import 'presentation/screens/cats_screen.dart';
 
 class AppRouter {
-  Route? generateRoute(RouteSettings settings){
+  late CatsRepository catsRepository;
+  late CatsCubit catsCubit;
+
+  AppRouter() {
+    catsRepository = CatsRepository(CatsWebService());
+    catsCubit = CatsCubit(catsRepository);
+  }
+
+  Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => const CatsScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => catsCubit,
+                  child: const CatsScreen(),
+                ));
+
       case catsScreen:
-        return MaterialPageRoute(builder: (_) => const CatsScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+              create: (context) => catsCubit,
+              child: const CatsScreen(),
+            ));
+
       case catsDetailsScreen:
         return MaterialPageRoute(builder: (_) => const CatsDetailsScreen());
       // default:
       //   return MaterialPageRoute(builder: (_) => const PageNotFound());
     }
+    return MaterialPageRoute(builder: (_) => const ErrorPage());
   }
 }
+
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('error page');
+  }
+}
+
